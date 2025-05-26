fbaba4c42157b1106c322eb8a0e8266c603e1ba8
Denis <den_makarcev@mail.ru>
2025-05-26 06:31:03 +0300

    update port for mysql

--- etl/kafka_to_clickhouse.py
+++ etl/kafka_to_clickhouse.py
@@ -1,12 +1,21 @@
 
 from pyspark.sql import SparkSession
-
-spark = SparkSession.builder     .appName("KafkaToClickHouse")     .getOrCreate()
-
-df = spark.read     .format("kafka")     .option("subscribe", "dbserver1.bank.orders")     .option("kafka.bootstrap.servers", "localhost:9092")     .load()
-
 from pyspark.sql.functions import col
+
+spark = SparkSession.builder.appName("KafkaToClickHouse").getOrCreate()
+
+df = spark.read.format("kafka") \
+                .option("subscribe", "dbserver1.bank.orders") \
+                .option("kafka.bootstrap.servers", "localhost:9092") \
+                .load()
+
+
 df_parsed = df.selectExpr("CAST(value AS STRING)")
 
 # Можно здесь добавить from_json + schema, фильтрацию и запись
-df_parsed.write     .format("jdbc")     .option("url", "jdbc:clickhouse://localhost:8123")     .option("dbtable", "orders_raw")     .option("user", "default")     .save()
+df_parsed.write.format("jdbc") \
+                .option("url", "jdbc:clickhouse://localhost:8123") \
+                .option("dbtable", "orders_raw") \
+                .option("user", "default") \
+                .save()
+
