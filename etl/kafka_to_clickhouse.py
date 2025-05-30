53d7d5b2be9b9d73cd0889a558c5306b3bf13255
Denis <den_makarcev@mail.ru>
2025-05-30 08:48:43 +0300

    asdasdad

--- etl/kafka_to_clickhouse.py
+++ etl/kafka_to_clickhouse.py
@@ -2,25 +2,22 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
-# SparkSession без Delta
+
 spark = SparkSession.builder \
     .appName("KafkaToClickhouse") \
     .getOrCreate()
 
-# Схема данных
 schema = StructType([
     StructField("id", IntegerType()),
     StructField("name", StringType()),
     StructField("email", StringType())
 ])
 
-# Чтение из Kafka
 df_raw = spark.read.format("kafka") \
     .option("kafka.bootstrap.servers", "host.docker.internal:9092") \
     .option("subscribe", "dbserver1.bank.customers") \
     .load()
 
-# Преобразование JSON
 df_json = df_raw.selectExpr("CAST(value AS STRING) as json_str") \
     .select(from_json(col("json_str"), StructType([
         StructField("payload", StructType([
@@ -30,10 +27,10 @@ df_json = df_raw.selectExpr("CAST(value AS STRING) as json_str") \
         ]))
     ])).alias("data"))
 
-# Распаковка payload
+
 df_final = df_json.select("data.payload.*")
 
-# Запись в ClickHouse
+
 df_final.write \
     .format("jdbc") \
     .option("driver", "com.clickhouse.jdbc.ClickHouseDriver") \
